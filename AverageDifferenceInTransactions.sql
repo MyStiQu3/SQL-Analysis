f0007a7ac9ab8111185ed79a18e6aa4744703f75
Ankit Dhingra <ankit@ankit-Latitude-5580>
2018-03-01 18:25:59 +0530

    Average Time Difference between any two consecutive transactions by a user.

--- AverageDifferenceInTransactions.sql
+++ AverageDifferenceInTransactions.sql
@@ -11,13 +11,13 @@ FROM
         firstHit.transactionTime firstTS,
         nexthit.transactionTime nextTS
 FROM (SELECT userId,transactionTime,transactionId,productId, RANK() OVER (PARTITION BY userId ORDER BY transactionTime ASC) AS RN 
-           FROM greedydata.hitdata
+           FROM dataset.transactionTable
            WHERE _PARTITIONTIME = TIMESTAMP("2018-02-26")
            AND gameId = "80715286"
            AND sdkv = "8.8.1"
            GROUP BY userId,transactionTime,transactionId,productId) firstHit
 INNER JOIN (SELECT userId,transactionTime,transactionId,productId, RANK() OVER (PARTITION BY userId ORDER BY transactionTime ASC) AS RN 
-           FROM greedydata.hitdata
+           FROM dataset.transactionTable
            WHERE _PARTITIONTIME = TIMESTAMP("2018-02-26")
            AND gameId = "80715286"
            AND sdkv = "8.8.1"
